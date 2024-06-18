2e479e98a37cf843e61cad8569aede49e3c7c61a
Jeff Foxx <foxx@adobe.com>
2024-06-18 13:55:52 -0400

    gave up linting and ignored

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -202,8 +202,6 @@ function decorateNavItem(parent, navSectionSearchItem) {
   parent.children[1].remove();
 }
 
-
-
 /**
  * loads and decorates the header, mainly the nav
  * @param {Element} block The header block element
